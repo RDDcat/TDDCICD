191e16f42374c6242372bbd065eacc5e0d75fd3c
RDDcat <yukmaro@gmail.com>
2022-10-19 03:10:54 +0900

    [UPDATE] 테스트 조건 업데이트 길이 8 미만 검사 추가

--- cicd/cicd/src/test/java/maro/study/cicd/PasswordMeter.java
+++ cicd/cicd/src/test/java/maro/study/cicd/PasswordMeter.java
@@ -4,8 +4,12 @@ public class PasswordMeter {
     public PasswordStrength meter(String pw) {
         if(pw == null || pw.isEmpty())
             throw new IllegalArgumentException();
-        if("abcC123".equals(pw))
+
+        boolean lengthRule = pw.length() >=8;
+        if(!lengthRule){
             return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
     }
 }
--- cicd/cicd/src/test/java/maro/study/cicd/PasswordMeterTest.java
+++ cicd/cicd/src/test/java/maro/study/cicd/PasswordMeterTest.java
@@ -24,18 +24,23 @@ public class PasswordMeterTest {
                 .isThrownBy(()-> passwordMeter.meter(""));
     }
 
+    private void assertPasswordStrength(String password, PasswordStrength expected) {
+        PasswordStrength result = passwordMeter.meter(password);
+        assertThat(result).isEqualTo(expected);
+    }
+
     @DisplayName("모든 조건을 충족하면 강함")
     @Test
     void meetAllRules() {
-        PasswordStrength result = passwordMeter.meter("abcABC123");
-        assertThat(result).isEqualTo(PasswordStrength.STRONG);
-        PasswordStrength result2 = passwordMeter.meter("123abcABC");
-        assertThat(result2).isEqualTo(PasswordStrength.STRONG);
+        assertPasswordStrength("abcABC123", PasswordStrength.STRONG);
+        assertPasswordStrength("123abcABC", PasswordStrength.STRONG);
     }
+
     @DisplayName("길이가 8미만, 다른 조건은 충족하는 경우")
     @Test
     void digitAndUppercase() {
-        PasswordStrength result = passwordMeter.meter("abcC123");
-        assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+        assertPasswordStrength("abcC123", PasswordStrength.NORMAL);
+        assertPasswordStrength("123abcC", PasswordStrength.NORMAL);
+        assertPasswordStrength("Cabc12", PasswordStrength.NORMAL);
     }
 }
